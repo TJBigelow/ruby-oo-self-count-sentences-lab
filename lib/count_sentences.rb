5788a8c7096fae9093117125a1cdbd91e33a4014
tjbigelow <tjbigelow@att.net>
2020-09-15 16:57:41 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] === '.'
   end
 
   def question?
-
+    self[-1] === '?'
   end
 
   def exclamation?
-
+    self[-1] === '!'
   end
 
   def count_sentences
-
+    @arr = self.split(/\.|!|\?/)
+    (@arr.reject {|str| str === ''}).count
   end
 end
